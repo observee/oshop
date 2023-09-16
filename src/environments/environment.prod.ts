6bedcf0fab551e54e4712f81148d288ad50aaf18
observee <observee@gmail.com>
2023-09-17 00:34:54 +0800

    test

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyCdkJ74vuldKaXu4paIb4uWCfbcqAJCfe4",
+    authDomain: "oshop-fdaa7.firebaseapp.com",
+    projectId: "oshop-fdaa7",
+    storageBucket: "oshop-fdaa7.appspot.com",
+    messagingSenderId: "149101903042",
+    appId: "1:149101903042:web:ff21533865b71d89390f5b",
+    measurementId: "G-9MPE1XWXCG"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyCdkJ74vuldKaXu4paIb4uWCfbcqAJCfe4",
+    authDomain: "oshop-fdaa7.firebaseapp.com",
+    projectId: "oshop-fdaa7",
+    storageBucket: "oshop-fdaa7.appspot.com",
+    messagingSenderId: "149101903042",
+    appId: "1:149101903042:web:ff21533865b71d89390f5b",
+    measurementId: "G-9MPE1XWXCG"
+  }
 };
 
 /*
